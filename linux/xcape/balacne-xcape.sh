161974f59992eea442f796024642aa83388f7329
rbritton <git@rbritton.dev>
2020-07-19 00:43:19 -0400

    adds working implementation

---
+++ linux/xcape/balacne-xcape.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+xcape -e "Hyper_L=ISO_Level3_Shift|Tab;ISO_Level3_Shift=ISO_Level3_Shift|Return;Shift_R=ISO_Level3_Shift|Escape;ISO_Level5_Shift=ISO_Level3_Shift|BackSpace;Hyper_R=ISO_Level3_Shift|Delete" $@
